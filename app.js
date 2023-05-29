46e36678a87138165e24cc1d857d072bfcfd923a
Manoj <2020uee1424@mnit.ac.in>
2023-05-30 01:53:38 +0530

    start our first node js

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const bodyParser = require("body-parser");
 const https = require("https");
 const ejs = require("ejs");
 const app = express();
-app.request(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.get("/airbnb", function (req, res) {
+    res.send("hello");
+})
+app.listen(3000);
